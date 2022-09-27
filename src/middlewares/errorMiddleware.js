5e77c776f7fd67b61f2aacfa986e6041ebd42dc0
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-27 23:01:35 +0300

    Added middlewares

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,19 @@
+const { ApiError } = require('../exceptions/ApiError');
+
+function errorMiddleware(error, req, res, next) {
+  if (error instanceof ApiError) {
+    const { status, message, errors } = error;
+
+    res.status(status).send({ message, errors });
+
+    return;
+  }
+
+  res.status(500).send({
+    message: 'Unexpected error',
+  });
+}
+
+module.exports = {
+  errorMiddleware,
+};
