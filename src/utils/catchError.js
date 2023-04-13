d95950ad09be3b37c4a186e431f912bc5e473039
Maxim Kantsura <maxim@smoothr.de>
2023-04-14 01:13:29 +0300

    add forgot files

---
+++ src/utils/catchError.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const catchError = (action) => {
+  return async(req, res, next) => {
+    try {
+      await action(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = { catchError };
