254309fe839777fa42d8dccce970658d255060d1
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-11-18 16:39:52 +0100

    fix: Linter fixed

--- src/setup.js
+++ src/setup.js
@@ -1,7 +1,5 @@
-"use strict";
+'use strict';
 
-const { User } = require("./models/User.js");
-const { Token } = require("./models/Token.js");
-const { sequelize } = require("./utils/db.js");
+const { sequelize } = require('./utils/db.js');
 
 sequelize.sync({ force: true });
