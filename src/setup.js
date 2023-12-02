cf51988400b1783cff0b9e4861c2064979026e3a
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-12-02 17:35:38 +0200

    fixed

--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,10 @@
-import 'dotenv/config';
-import { sequelize } from './utils/db.js';
-import './models/User.js';
-import './models/Token.js';
+'use strict';
+
+require('dotenv').config();
+
+const { sequelize } = require('./utils/db');
+
+require('./models/User.js');
+require('./models/Token.js');
 
 sequelize.sync({ force: true });
