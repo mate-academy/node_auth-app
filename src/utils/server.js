5e32e15c8609ac0035d4b8bccb71c74f0475e298
Zibi95 <sylwester.sob95@gmail.com>
2023-12-18 11:33:38 +0100

    connect database

--- src/utils/server.js
+++ src/utils/server.js
@@ -3,10 +3,16 @@
 
 const express = require('express');
 
+const { sequelize } = require('./db.js');
+
 const createServer = (PORT) => {
   const app = express();
 
   app.listen(PORT, () => {
+    sequelize
+      .authenticate()
+      .then(() => console.log('Database ON'))
+      .catch(() => console.log('Could not connect database'));
     console.log(`Listening on port ${PORT}`);
   });
 
