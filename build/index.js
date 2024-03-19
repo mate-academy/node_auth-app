acb03552239b1f501d989f483fd967fc1515b094
Softjey <misivsvatoslav@gmail.com>
2024-03-19 23:26:30 +0100

    delete build from git

--- build/index.js
+++
@@ -1,9 +0,0 @@
-var _a;
-import 'dotenv/config';
-import { createServer } from './app.js';
-const app = createServer();
-const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
-app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
