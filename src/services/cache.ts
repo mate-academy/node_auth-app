4cb3690e135dd4b39c75938344616b1ab8e08339
Softjey <misivsvatoslav@gmail.com>
2024-03-25 16:49:50 +0100

    change redis to locale server

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,10 +1,9 @@
 import { createClient } from 'redis';
 
 export const redisClient = createClient({
-  password: process.env.REDIS_PASSWORD ?? 'WllM8NGdOK4F5cvvWqP9SpIoMnnlZirZ',
   socket: {
-    host: process.env.REDIS_HOST ?? 'redis-18295.c300.eu-central-1-1.ec2.cloud.redislabs.com',
-    port: +(process.env.REDIS_PORT ?? 18295),
+    host: process.env.REDIS_HOST ?? 'localhost',
+    port: +(process.env.REDIS_PORT ?? '6379'),
   },
 });
 
