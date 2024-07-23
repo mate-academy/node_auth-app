7f8658242a987b4f77d4385a80e561100d1b875a
Olaf <olaf.chuszno@gmail.com>
2024-07-23 17:26:52 +0200

    Remove console logs

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -5,28 +5,19 @@ import { parseAccessToken } from '../utils/parseAccessToken.js';
 export const authMiddleware = (req, res, next) => {
   const auth = req.headers.authorization;
 
-  const headers = req.headers;
-
-  console.log('headers:'.headers);
-
-  console.log('auth (middleware):', auth);
-
   if (!auth) {
-    console.log('---------1 auth-middleware-------');
     throw ApiError.Unauthorized();
   }
 
   const accessToken = parseAccessToken(auth);
 
   if (!accessToken) {
-    console.log('---------2-------');
     throw ApiError.Unauthorized();
   }
 
   const result = verifyAccessToken(accessToken);
 
   if (result === null) {
-    console.log('---------3-------');
     throw ApiError.Unauthorized();
   }
 
