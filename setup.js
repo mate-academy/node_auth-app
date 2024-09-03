f0dcd1a03ff7499f278357cb283a83c0064bb929
deezhizyu <kalenskuja@gmail.com>
2024-09-03 14:02:53 +0200

    Add package.json

--- setup.js
+++ setup.js
@@ -1,5 +1,6 @@
-const { User } = require('./src/models/User.model');
+require('./src/models/Token.model');
+require('./src/models/User.model');
 
-User.sync({
-  force: true,
-});
+const { client } = require('./src/utils/db');
+
+client.sync({ force: true });
