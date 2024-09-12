49baa7d35230997b8c5b094ba641ef98b5cdf6ca
Hanna Svirina <26anna07@ukr.net>
2024-09-12 22:44:49 +0200

    solution

---
+++ src/services/jwt.service.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+const sign = (user) => {
+  const token = jwt.sign(user, process.env.JWT_KEY, {
+    expiresIn: '5s',
+  });
+
+  return token;
+};
+
+function verify(token) {
+  try {
+    return jwt.verify(token, process.env.JWT_KEY);
+  } catch (error) {
+    return null;
+  }
+}
+
+const signRefresh = (user) => {
+  const token = jwt.sign(user, process.env.JWT_REFRESH_KEY);
+
+  return token;
+};
+
+const verifyRefresh = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_REFRESH_KEY);
+  } catch (error) {
+    return null;
+  }
+};
+
+export const jwtService = {
+  sign,
+  verify,
+  signRefresh,
+  verifyRefresh,
+};
