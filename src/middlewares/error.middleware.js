22cf938ec07f411b2d72652b3c2feb6158302b88
MarekSax <mareksax@wp.pl>
2024-10-20 12:20:45 +0200

    feat: add error logging

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { ApiError } from '../exceptions/api.error.js';
 
@@ -5,8 +6,12 @@ export const errorMiddleware = (error, req, res, next) => {
   if (error instanceof ApiError) {
     const { status, message, errors } = error;
 
+    console.error('Handled API error:', error.message, error.errors);
+
     return res.status(status).send({ message, errors });
   }
+
+  console.error('Unhandled error:', error);
 
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
