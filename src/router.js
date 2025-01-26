79617c162eb70a62bf548df33edd88b3af483683
valentyn19939 <valik.hai1993@gmail.com>
2025-01-26 13:29:49 +0100

    add task solution

---
+++ src/router.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { authController } from './conrollers/auth.controller';
+import { profileController } from './conrollers/profile.controller';
+import { authenticateUser } from './midlleware/authenticateUser';
+
+const authRouter = new express.Router();
+
+authRouter.post('/register', authController.register);
+
+authRouter.get('/verify-email', authController.verifyEmail);
+
+authRouter.post('/login', authController.login);
+
+authRouter.post('/logout', authController.logout);
+
+authRouter.post('/reset-password', authController.resetPassword);
+authRouter.post('/confirm-reset-password', authController.confirmResetPassword);
+
+authRouter.get('/profile', authenticateUser, profileController.viewProfile);
+
+authRouter.put('/profile', authenticateUser, profileController.updateProfile);
+
+authRouter.put(
+  '/change-password',
+  authenticateUser,
+  profileController.changePassword,
+);
+
+authRouter.put(
+  '/change-email',
+  authenticateUser,
+  profileController.changeEmail,
+);
+
+authRouter.all('*', (req, res) => {
+  res.status(404).json({ error: 'Page not found' });
+});
+
+export default authRouter;
