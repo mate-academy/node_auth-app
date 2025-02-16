71567c7fc33af9011e0b84251eedf03dba00d1c2
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2025-02-17 00:49:51 +0200

    updated connection to the db for deploy

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,8 +5,15 @@ dotenv.config();
 
 export const client = new Sequelize({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
