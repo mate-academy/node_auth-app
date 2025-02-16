8d5e33b52c20f4408de3ef02398ace0ed4b688c6
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2025-02-17 01:15:27 +0200

    fixed setup.js

--- src/setup.js
+++ src/setup.js
@@ -2,5 +2,5 @@ import 'dotenv/config';
 import { User } from './models/user.model.js';
 import { Token } from './models/token.model.js';
 
-Token.sync({ force: true });
-User.sync({ force: true });
+await User.sync({ force: true });
+await Token.sync({ force: true });
