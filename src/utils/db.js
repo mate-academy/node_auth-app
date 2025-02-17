4538b16da781d227fd2be3ff604f90ca41e1580d
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2025-02-17 03:04:53 +0200

    updated db.js to run server locally

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,19 +1,18 @@
+/*eslint-disable*/
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 export const client = new Sequelize({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+  port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+  dialectOptions: process.env.DB_HOST?.includes('localhost')
+    ? {}
+    : {
+        ssl: { require: true, rejectUnauthorized: false },
+      },
 });
